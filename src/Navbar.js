7adbf7cc1463859016b7e87658574c3f566153f2
Yasmin <yasminxismail@gmail.com>
2025-09-20 12:49:30 +0100

    Add Navbar.js and Home.js components

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+{/*stateless funcuitonal component, template for navbar: */}
+const Navbar = () => {
+    return (
+    <nav className="navbar">
+        <h1>The Dojo Blog</h1>
+
+        <div className="links"></div>
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+
+    </nav>
+    );
+}
+ 
+export default Navbar;
+
