e48aabe03875b444a9c6492d506d16ec6624e846
Yasmin <yasminxismail@gmail.com>
2025-09-22 15:27:38 +0100

    add blog (objects for blog, mapping through blog) and add style for blog

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState} from 'react';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]); 
+
+
     return (  
         <div className="home">
-            <h2>Home Page</h2>
+            {/*fires a call back for each item, each item around we want to return jsx template & that's going to go inside parentheses*/ }        
+            <p> {blogs.map((blog) => ( 
+                <div className="blog-preview" key={blog.id}> 
+                    <h2> {blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}</p>   
         </div>
     );
 }
