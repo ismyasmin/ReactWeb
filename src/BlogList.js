8f904f2884153fdbfa47fba8b99c2ec9d5bfc386
Yasmin <yasminxismail@gmail.com>
2025-09-23 18:49:27 +0100

    add props for BlogList.js - can be reused

---
+++ src/BlogList.js
@@ -0,0 +1,16 @@
+const  BlogList= ({blogs, title}) => {
+    return ( 
+        <div className="blog-list">
+            <h2> {title}</h2>
+        {/*fires a call back for each item, each item around we want to return jsx template & that's going to go inside parentheses*/ }        
+        {blogs.map((blog) => ( 
+            <div className="blog-preview" key={blog.id}> 
+                <h2> {blog.title}</h2>
+                <p>Written by {blog.author}</p>
+            </div>
+        ))} 
+        </div>
+    );
+}
+ 
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -10,13 +11,7 @@ const Home = () => {
 
     return (  
         <div className="home">
-            {/*fires a call back for each item, each item around we want to return jsx template & that's going to go inside parentheses*/ }        
-            <p> {blogs.map((blog) => ( 
-                <div className="blog-preview" key={blog.id}> 
-                    <h2> {blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}</p>   
+            <BlogList blogs = {blogs} title ="All Blogs"/> {/* Property name in the tag with dynamic value*/}
         </div>
     );
 }
