05cf10f8cd4f97bee8a1f83e2540627e53d7ee62
Yasmin <yasminxismail@gmail.com>
2025-09-24 08:12:05 +0100

    Add delete function (function as props). Add delete button

--- src/Home.js
+++ src/Home.js
@@ -8,10 +8,17 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]); 
 
+    const handleDelete = (id) => 
+        {
+            const newBlogs = blogs.filter(blog => blog.id !== id);{/*to store the filtered array temporarily*/}
+            setBlogs(newBlogs);
+        }
+
 
     return (  
         <div className="home">
-            <BlogList blogs = {blogs} title ="All Blogs"/> {/* Property name in the tag with dynamic value*/}
+            <BlogList blogs = {blogs} title ="All Blogs" handleDelete={handleDelete}/> {/* Property name in the tag with dynamic value*/}
+            {/* <BlogList blogs = {blogs.filter((blog) => blog.author === 'mario')} title ="Mario's Blogs"/> fires a callback function for each item in the array, if it returns true, it keeps it in the array. if false, it filters out the array & returns a new array with only the items that we dont filter out of it included & we're passing that data in then as a prop */}
         </div>
     );
 }
