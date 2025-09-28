133f4258f6c74352bb3d64e612bb0b9bfe2820eb
Yasmin <yasminxismail@gmail.com>
2025-09-28 04:02:46 +0100

    Conditional Loading Message - Loading message while data is being fetched

--- src/Home.js
+++ src/Home.js
@@ -3,25 +3,31 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null); 
-
-    const [name, setName] = useState('mario');
+    const [isLoading, setIsLoading] = useState(true);
+ 
+    // const [name, setName] = useState('mario');
 
         useEffect(() =>
-            {
-                fetch('http://localhost:8000/blogs') //Endpoint: http://localhost:8000/blogs
-                .then(res =>{
-                    return res.json()
-                })
-                .then((data) =>
+            { //setTimeout to simulate a real request that might be to some other server on the internet  
+                setTimeout(() =>
                 {
-                    console.log(data);
-                    setBlogs(data)
-                })
+                    fetch('http://localhost:8000/blogs') //Endpoint: http://localhost:8000/blogs
+                        .then(res =>{
+                            return res.json()
+                        })
+                        .then((data) =>
+                        {
+                            // console.log(data);
+                            setBlogs(data)
+                            setIsLoading(false);
+                        }); // function fires after 1 second then performs the fetch (dont do for a real world application, forcing user to wait an addtional second, only to simulate to make it a bit more realistic)
+                },1000);
             },[]);
             
 
     return (  
         <div className="home">
+            {isLoading && <div>Loading...</div>}
             {/*conditonal templating, logical evaluates the left first & if true, evaluates the right then outputs. - how to conditionally output parts of templates*/}
             {blogs && <BlogList blogs = {blogs} title ="All Blogs"/>}  {/*Property name in the tag with dynamic value*/}
         </div>
