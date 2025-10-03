d42245c75421c72bc5447698db90fc0ad5621aeb
Yasmin <yasminxismail@gmail.com>
2025-10-04 00:32:36 +0100

    Showing data for that single blog when clicked on Home page by reusing custom hook use fetch. Use UseFetch hook in the blog details component and pass in the url of the endpoint that will  fetch data. Style added for the data that's been fetched and displayed.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const {id} = useParams(); // allows to grab route parameters from the route
+    const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/' + id);
     return (  
         <div className="blog-details">
-            <h2>Blog details -  { id } </h2>
+            {/* <h2>Blog details -  { id } </h2> */}
+            {isPending && <div>Loading...</div>} {/*lgical AND: thing on the right only outputs if the thing on the left is right*/}
+            {error && <div>{error} </div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     );
 }
