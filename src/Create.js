70653cabc43b39270ce484ff178a727a6ed5a7b0
Yasmin <yasminxismail@gmail.com>
2025-10-04 12:32:10 +0100

    Send a post request to an endpoint that json server provides so the object is added to the data. Add loading message.

--- src/Create.js
+++ src/Create.js
@@ -4,15 +4,28 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('yoshi');
+    const [isPending, setIsPending] = useState(false); /*set to false because when page is first loaded, not making a request,
+     only after form is submitted */
 
     const handleSubmit = (e) =>{
         e.preventDefault(); // prevents the page from being refreshed when button is clicked on
         //no need to create an id when a post request is made as json server will automatically create a unique id
         const blog = {title, body, author}; //blog object
 
-        console.log(blog);
+        setIsPending(true);
+
+        // asynchronous & returns a promise; make a post request to this endpoint to add in a new blog. json adds in unique ie.
+        fetch('http://localhost:8000/blogs', {  // second argument get the data and define the type of request is sending
+            method: 'POST',
+            headers: {"Content-Type": "application/json"}, // telling server the type of content sending with this request is json data
+            body: JSON.stringify(blog) // change object into a json string
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+        })
+     
     }
-    return (
+    return ( 
         <div className="create">
             <h2>Add a Blog</h2>
             <form onSubmit={handleSubmit}>
@@ -39,7 +52,8 @@ const Create = () => {
                     <option value="yoshi">yoshi</option>
                 </select>
 
-                <button>Add Blog</button>
+                { !isPending && <button>Add Blog</button> }
+                { isPending && <button disabled>Adding Blog...</button> }
                 <p>{title}</p>
                 <p>{body}</p>
                 <p>{author}</p>
