5fa2b919d82e132665cc0ae94c5313ff3827ccdd
Yasmin <yasminxismail@gmail.com>
2025-10-04 12:10:10 +0100

    A blog object from state values, capture form data, put in an object, handle in React instead of reloading page.

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,18 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('yoshi');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault(); // prevents the page from being refreshed when button is clicked on
+        //no need to create an id when a post request is made as json server will automatically create a unique id
+        const blog = {title, body, author}; //blog object
+
+        console.log(blog);
+    }
     return (
         <div className="create">
             <h2>Add a Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                     type="text"
