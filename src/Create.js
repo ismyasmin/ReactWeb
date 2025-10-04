87a1d9be13754d53519f5a180805220db67be085
Yasmin <yasminxismail@gmail.com>
2025-10-04 11:55:13 +0100

    Create.js user inputting a new blog. Setting up input fields in forms to track their values. Style for form added.

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,41 @@
+import {useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('yoshi');
     return (
         <div className="create">
             <h2>Add a Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title} 
+                    onChange={(e) => setTitle(e.target.value)}//change title when input value is changed. Target is this input
+                />
+
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+
+                <label>Blog author:</label>
+                <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
 // import from react-dom/client instead of react-dom
 // Use ReactDOM.createRoot() instead of ReactDOM.render().
